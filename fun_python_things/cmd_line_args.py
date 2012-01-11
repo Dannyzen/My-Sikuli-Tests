5d234398fdc583173e9c3cc99addbe8cf1b07acb
Danny Rosen <danny.rosen@gmail.com>
2012-01-11 01:04:31 -0500

    args

--- fun_python_things/cmd_line_args.py
+++ fun_python_things/cmd_line_args.py
@@ -1,6 +1,8 @@
 #http://stackoverflow.com/questions/4117530/sys-argv1-meaning-in-script
+#look at http://stackoverflow.com/questions/7427101/dead-simple-argparse-example-wanted-1-argument-3-results for ideas on using arparse
+
 
 import sys
 for arg in sys.argv:
 	argz = sys.argv[1:]  #ignore sys.argv[0] which is default the scriptname 
-print (argz[1])
+print (argz[0],argz[1])
